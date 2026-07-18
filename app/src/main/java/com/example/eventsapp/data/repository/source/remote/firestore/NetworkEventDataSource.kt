61ffe6917a194982f78dec716bc63681a42420c4
Anshu230806 <ay0354760@gmail.com>
2026-07-18 13:34:41 +0530

    no change

--- app/src/main/java/com/example/eventsapp/data/repository/source/remote/firestore/NetworkEventDataSource.kt
+++ app/src/main/java/com/example/eventsapp/data/repository/source/remote/firestore/NetworkEventDataSource.kt
@@ -7,4 +7,5 @@ interface NetworkEventDataSource {
 // Asynchronously fetches a list of events from a remote API and returns them as NetworkEvent data objects.
     suspend fun loadEvents() : List<NetworkEvent>
 
+
 }
